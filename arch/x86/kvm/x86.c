855b1aa06f8d0d9ae9fb75e72e16ab9fa194eb81
Oliver Upton <oliver.upton@linux.dev>
2023-03-20 01:19:54 +0000

    entry/kvm: Have xfer_to_guest_mode_work_pending() flush rcuog wakeup
    Both arm64 and riscv usage of the generic guest entry infra fail to
    flush pending rcuog wakeups before the last scheduling point.
    xfer_to_guest_mode_work_pending() is the last scheduling point before
    guest entry across all architectures, and in every case it is called
    exactly once. Thus, there is little value in having a separate helper to
    perform the deferred wakeup.
    
    Fix both of the messed up arches (and avoid repeated failures future
    adoption) by processing the deferred wakeup implicitly in
    xfer_to_guest_mode_work_pending().
    
    Fixes: 6caa5812e2d1 ("KVM: arm64: Use generic KVM xfer to guest work function")
    Fixes: 9c00fbdd93a2 ("RISC-V: KVM: Use generic guest entry infrastructure")
    Cc: Jisheng Zhang <jszhang@kernel.org>
    Cc: Anup Patel <anup@brainfault.org>
    Cc: stable@vger.kernel.org
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -2115,7 +2115,6 @@ EXPORT_SYMBOL_GPL(kvm_emulate_monitor);
 
 static inline bool kvm_vcpu_exit_request(struct kvm_vcpu *vcpu)
 {
-	xfer_to_guest_mode_prepare();
 	return vcpu->mode == EXITING_GUEST_MODE || kvm_request_pending(vcpu) ||
 		xfer_to_guest_mode_work_pending();
 }
--- include/linux/entry-kvm.h
+++ include/linux/entry-kvm.h
@@ -52,20 +52,6 @@ static inline int arch_xfer_to_guest_mode_handle_work(struct kvm_vcpu *vcpu,
 int xfer_to_guest_mode_handle_work(struct kvm_vcpu *vcpu);
 
 /**
- * xfer_to_guest_mode_prepare - Perform last minute preparation work that
- *				need to be handled while IRQs are disabled
- *				upon entering to guest.
- *
- * Has to be invoked with interrupts disabled before the last call
- * to xfer_to_guest_mode_work_pending().
- */
-static inline void xfer_to_guest_mode_prepare(void)
-{
-	lockdep_assert_irqs_disabled();
-	tick_nohz_user_enter_prepare();
-}
-
-/**
  * __xfer_to_guest_mode_work_pending - Check if work is pending
  *
  * Returns: True if work pending, False otherwise.
@@ -92,6 +78,7 @@ static inline bool __xfer_to_guest_mode_work_pending(void)
 static inline bool xfer_to_guest_mode_work_pending(void)
 {
 	lockdep_assert_irqs_disabled();
+	tick_nohz_user_enter_prepare();
 	return __xfer_to_guest_mode_work_pending();
 }
 #endif /* CONFIG_KVM_XFER_TO_GUEST_WORK */
