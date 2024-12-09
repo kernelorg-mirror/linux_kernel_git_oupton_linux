9dc80b6b3f17bb7e53fb720e88876d50781390a3
Oliver Upton <oliver.upton@linux.dev>
2024-12-09 10:09:18 -0800

    KVM: arm64: Remove debug tracepoints
    The debug tracepoints are a useless firehose of information that track
    implementation detail rather than well-defined events. These are going
    to be rather difficult to uphold now that the implementation is getting
    redone, so throw them out instead of bending over backwards.
    
    Tested-by: James Clark <james.clark@linaro.org>
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -657,8 +657,6 @@ static bool trap_debug_regs(struct kvm_vcpu *vcpu,
 		vcpu_set_flag(vcpu, DEBUG_DIRTY);
 
 	kvm_debug_set_guest_ownership(vcpu);
-	trace_trap_reg(__func__, r->reg, p->is_write, p->regval);
-
 	return true;
 }
 
@@ -711,8 +709,7 @@ static bool trap_bvr(struct kvm_vcpu *vcpu,
 	else
 		dbg_to_reg(vcpu, p, rd, dbg_reg);
 
-	trace_trap_reg(__func__, rd->CRm, p->is_write, *dbg_reg);
-
+	kvm_debug_set_guest_ownership(vcpu);
 	return true;
 }
 
@@ -747,8 +744,6 @@ static bool trap_bcr(struct kvm_vcpu *vcpu,
 		reg_to_dbg(vcpu, p, rd, dbg_reg);
 	else
 		dbg_to_reg(vcpu, p, rd, dbg_reg);
-
-	trace_trap_reg(__func__, rd->CRm, p->is_write, *dbg_reg);
 
 	return true;
 }
@@ -785,9 +780,6 @@ static bool trap_wvr(struct kvm_vcpu *vcpu,
 	else
 		dbg_to_reg(vcpu, p, rd, dbg_reg);
 
-	trace_trap_reg(__func__, rd->CRm, p->is_write,
-		vcpu->arch.vcpu_debug_state.dbg_wvr[rd->CRm]);
-
 	return true;
 }
 
@@ -822,8 +814,6 @@ static bool trap_wcr(struct kvm_vcpu *vcpu,
 		reg_to_dbg(vcpu, p, rd, dbg_reg);
 	else
 		dbg_to_reg(vcpu, p, rd, dbg_reg);
-
-	trace_trap_reg(__func__, rd->CRm, p->is_write, *dbg_reg);
 
 	return true;
 }
