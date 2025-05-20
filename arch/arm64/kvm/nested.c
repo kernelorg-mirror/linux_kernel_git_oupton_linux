07a3db927e1e36f643ce50b0ea3752c785588126
Oliver Upton <oliver.upton@linux.dev>
2025-05-20 12:42:10 -0700

    KVM: arm64: nv: Advertise FEAT_nTLBPA
    FEAT_nTLBPA indicates an implementation doesn't have a noncoherent
    translation cache indexed by output address. Since all of KVM's
    translation caches are indexed by input address (e.g. VNCR TLB, shadow
    stage-2 MMUs) it is safe to claim support for this feature.
    
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>

--- arch/arm64/kvm/nested.c
+++ arch/arm64/kvm/nested.c
@@ -1495,7 +1495,6 @@ u64 limit_nv_id_reg(struct kvm *kvm, u32 reg, u64 val)
 			 ID_AA64MMFR1_EL1_XNX		|
 			 ID_AA64MMFR1_EL1_TWED		|
 			 ID_AA64MMFR1_EL1_ETS		|
-			 ID_AA64MMFR1_EL1_nTLBPA	|
 			 ID_AA64MMFR1_EL1_CMOW);
 
 		/* FEAT_E2H0 implies no VHE */
